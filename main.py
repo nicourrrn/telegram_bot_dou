a0167ce54d4a2a429e27c418c51df278a86b4361
Sorokin Vadym <s57111702@gmail.com>
2022-05-23 17:06:33 +0300

    Aiogram

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-import datetime
-
+from datetime import datetime, timedelta
 import asyncio
 
-import yaml
-import sqlalchemy.orm.query
-from telebot.async_telebot import AsyncTeleBot
-from telebot.types import Message
+from aiogram import Bot, Dispatcher, executor
+from aiogram.types import Message
+
 import database as db
 import parcer
 
-with open("config.yml", 'r') as file:
-    config = yaml.safe_load(file)
 
-bot_token = config["bot_token"]
-admin_ids = config["admin_ids"]
-bot = AsyncTeleBot(bot_token)
+bot_token = "5121135624:AAGYcoQmMDLElLGIS_Qo8F7DOGmeaFc3Kcs"
+bot = Bot(bot_token)
+dp = Dispatcher(bot)
 
 categories = {
     "Project Manager": ("Project+Manager", ""),
@@ -28,20 +24,21 @@ categories = {
 }
 
 
-@bot.message_handler(commands=['start'])
+@dp.message_handler(commands=['start'])
 async def start(message: Message):
     profession = message.text.replace("_", " ").split(" ")
     if len(profession) < 2:
-        await bot.reply_to(message, "Incorrect link, please, input command /start [YOUR_PROFESSION]")
+        await message.reply("Incorrect link, please, input command /start [YOUR_PROFESSION]")
         return
     if " ".join(profession[1:]) not in categories.keys():
-        await bot.reply_to(message, f"Profession {' '.join(profession[1:])} not found")
+        await message.reply(f"Profession {' '.join(profession[1:])} not found")
         return
-    await bot.reply_to(message, f"Hello, {message.from_user.first_name}, from work bot, you added to list")
+    await message.answer(f"Hello, {message.from_user.first_name}, from work bot, you added to list")
+
     new_client = db.Client(id=message.from_user.id,
                            category=" ".join(profession[1:]),
                            name=message.from_user.first_name,
-                           last_vacancy_time=datetime.datetime.now())
+                           last_vacancy_time=datetime.now() - timedelta(days=1))
     with db.Session() as session:
         if user := session.get(db.Client, message.from_user.id):
             user.category = new_client.category
@@ -51,37 +48,42 @@ async def start(message: Message):
         session.commit()
 
 
-@bot.message_handler(commands=['stop'])
+@dp.message_handler(commands=['stop'])
 async def stop(message: Message):
     with db.Session() as session:
         session.delete(session.get(db.Client, message.from_user.id))
         session.commit()
+        await message.answer("Bye!")
 
 
 async def send_vacancies():
+    session = db.Session()
+    for category, params in categories.items():
+        last_vacancy = (await parcer.get_feeds(*params))[0]
+        clients = session.query(db.Client) \
+            .filter(db.Client.category == category,
+                    db.Client.last_vacancy_time < last_vacancy.published_parsed)
+        for client in clients:
+            await bot.send_message(client.id, f"{last_vacancy['title']}\n{last_vacancy['link']}")
+            client.last_vacancy_time = datetime.now()
+            session.add(client)
+    session.commit()
+    session.close()
+
+
+async def send_vacancies_by_period(period=60):
     while True:
-        await asyncio.sleep(10)
-        session = db.Session()
-        for category, params in categories.items():
-            last_vacancy = (await parcer.get_feeds(*params))[0]
-            clients: sqlalchemy.orm.query.Query = session.query(db.Client) \
-                .filter(db.Client.category == category)
-                        # db.Client.last_vacancy_time < last_vacancy.published_parsed)
-            for client in clients:
-                await bot.send_message(client.id, last_vacancy["title"])
-                client.last_vacancy_time = datetime.datetime.now()
-                session.add(client)
-        session.commit()
-        session.close()
-        await asyncio.sleep(50)
+        try:
+            await send_vacancies()
+        except Exception as e:
+            print(e)
+        await asyncio.sleep(period)
 
 
 async def main():
-    tasks = [bot.polling(), send_vacancies()]
-    await asyncio.gather(*tasks)
-
+    await asyncio.gather(dp.start_polling(), send_vacancies_by_period(10))
 
 try:
-    asyncio.run(main())
+    executor.start(dp, main())
 except KeyboardInterrupt:
     print("Good bye")
